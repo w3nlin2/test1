d4c6027240b628aaf4618fc90cc74e6f49084d7d
w3nlin2 <52774438+w3nlin2@users.noreply.github.com>
2019-09-17 21:31:48 +0800

    Add files via upload

---
+++ precache-manifest.972361aaea0d71b6eff30d33bc794cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c933e318a0df84c91da76ae6e6c3d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d7caa6b7b69856897a",
+    "url": "/static/css/main.31dab251.chunk.css"
+  },
+  {
+    "revision": "50a8577484f4038acd15",
+    "url": "/static/js/2.f3947a27.chunk.js"
+  },
+  {
+    "revision": "f4d7caa6b7b69856897a",
+    "url": "/static/js/main.a5a70c13.chunk.js"
+  },
+  {
+    "revision": "2d2fcc775881e68f7e42",
+    "url": "/static/js/runtime~main.7c7af291.js"
+  }
+]);
